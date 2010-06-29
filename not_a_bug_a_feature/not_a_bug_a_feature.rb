1016dde96f84d40c994f7e981e28dbe0424c142a
Bertolami <roman@bertolami.net>
2010-06-29 21:17:09 +0200

    started with algorithm

--- not_a_bug_a_feature/not_a_bug_a_feature.rb
+++ not_a_bug_a_feature/not_a_bug_a_feature.rb
@@ -2,11 +2,48 @@
 
 class NotABugAFeature
   attr_accessor :transitions
-  def initialize 
+  attr_reader  :distance_to_goal
+  def initialize n
     @transitions = Array.new
-    
+    @open_states = Array.new
+    @closed_states = Hash.new
+    @open_states << State.new("-" * n, 0)
+    @distance_to_goal = -1
+    @goal_string = "+" * n
   end
  
+  def run
+    log @open_states
+    while @open_states.size > 0
+      state = @open_states.shift
+      if not contains_closer_existing state
+        @closed_states[state.state_string] = state
+      end
+      log state
+      @transitions.each do |transition|
+        next_state = state.next_state transition
+        if next_state
+          if (@goal_string == next_state.state_string)
+            if @distance_to_goal == -1 or @distance_to_goal > next_state.distance
+              @distance_to_goal = next_state.distance  
+            end
+          else
+            if not contains_closer_existing next_state
+              @open_states << next_state
+            end
+          end
+        end
+      end
+    end
+  end
+  def contains_closer_existing state
+      existing = @closed_states[state.state_string]
+      existing and existing.distance < state.distance
+  end
+  
+  def goal? state_string
+    @goal_string == state_string
+  end
   
 end
 
@@ -22,18 +59,20 @@ class Transition
 end
 
 class State
-  def initialize stateString
-    @stateString = stateString
+  attr_accessor :state_string, :distance
+  def initialize state_string, distance
+    @state_string = state_string
+    @distance = distance
   end
   
   def applies? transition
-    0.upto(@stateString.length-1) do |i|
-      return false if(not matchingBug(@stateString[i..i], transition.matcher[i..i]))    
+    0.upto(@state_string.length-1) do |i|
+      return false if(not matching_bug(@state_string[i..i], transition.matcher[i..i]))    
     end
     true
   end
   
-  def matchingBug(stateBug, matcherBug)
+  def matching_bug(stateBug, matcherBug)
     stateBug == matcherBug or matcherBug == "0"
   end
   
@@ -45,15 +84,19 @@ class State
      end
   end
   
-  def nextState transition
+  def next_state transition
     if(applies? transition) 
-      stateString = ""
-      0.upto(@stateString.length-1) do |i|
-        stateString << bug(@stateString[i..i], transition.applier[i..i])    
+      state_string = ""
+      0.upto(@state_string.length-1) do |i|
+        state_string << bug(@state_string[i..i], transition.applier[i..i])    
       end
+      State.new(state_string, @distance +1)
     else
       nil
     end
+  end
+  def to_s
+    "#{@state_string} (#{@distance})"
   end
 end
 
@@ -71,16 +114,17 @@ caze = 0
 while true
   exit if input[index] =~ /^0 0$/ 
   if input[index] =~ /^\d+\s+\d+$/  
-    engine = NotABugAFeature.new
     (n, m) = input[index].split(/\s+/).map{|x| x.to_i}
+    engine = NotABugAFeature.new n
     caze += 1
     1.upto(m) do |i|
       index += 1
       engine.transitions << Transition.new(input[index])
-      log input[index]
     end
+    
     puts "Product #{caze}"
-    puts "Hallo (#{n} #{m}) #{engine.transitions.join ' '}"
+    engine.run
+    puts "minimal distance #{engine.distance_to_goal}"
     index += 1
   end
 end
